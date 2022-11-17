5ba6bd9980030e2fcb9902470dcf9169cb637679
Jonathan Lara Segovia <112134499+JohnTravisCano69@users.noreply.github.com>
2022-11-16 16:16:19 -0800

    Update pico_c.c

--- pico_c.c
+++ pico_c.c
@@ -1,4 +1,3 @@
-,,,c
 /*Lara Segovia Jonathan
 19210515
 Lenguaje de interfaz
@@ -23,5 +22,4 @@ int main() {
     }
     return 0;
 }
-,,,
-[Ver video de como se creo el programa]: https://www.loom.com/share/5ca9de3637e1430ba72f553de7ea9380
+
